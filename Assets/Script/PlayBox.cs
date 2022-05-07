d4d38b7976903307067c223406340922f437ad6a
riversunlight <aabbee107@icloud.com>
2022-05-07 22:42:43 +0900

    playboxの動きを追加

---
+++ Assets/Script/PlayBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    // 操作に対する動き
+    void Move()
+    {
+        Vector3 pos = this.transform.position;
+        if (Input.GetKey(KeyCode.A)) pos.x -= 0.1f;
+        if (Input.GetKey(KeyCode.D)) pos.x += 0.1f;
+        this.transform.position = pos;
+    }
+}
